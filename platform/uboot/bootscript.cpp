GloDroid/glodroid_configuration

commit 4880d9ac5538b46f031e8c492b71ad93059de98c

bootscript: Rework to allow customization
Allow community devices to customize the behavior.

Signed-off-by: Roman Stratiienko <r.stratiienko@gmail.com>

platform/uboot/bootscript.cpp
@@ -7,26 +7,13 @@
  */
 
 #include "bootscript.h"
+#include "platform.h"
+#include "device.h"
 
-#ifdef platform_broadcom
-setenv dtbaddr 0x1fa00000
-setenv loadaddr 0x10008000
-setenv vloadaddr 0x13008000
-setenv dtboaddr 0x12008000
-#endif
-
-#ifdef platform_sunxi
-setenv dtbaddr 0x5fa00000
-setenv loadaddr 0x50008000
-setenv vloadaddr 0x53008000
-setenv dtboaddr 0x52008000
-#endif
-
-#ifdef platform_rockchip
-setenv dtbaddr 0x1fa00000
-setenv loadaddr 0x10008000
-setenv vloadaddr 0x13008000
-setenv dtboaddr 0x12008000
+#ifdef PLATFORM_SETUP_ENV
+PLATFORM_SETUP_ENV()
+#else
+#error PLATFORM_SETUP_ENV is not defined
 #endif
 
 setenv    main_fdt_id 0x100
@@ -35,8 +22,8 @@ setenv overlay_fdt_id 0xFFF
 /* EMMC cards have 512k erase block size. Align partitions accordingly to avoid issues with erasing. */
 
 setenv partitions "uuid_disk=\${uuid_gpt_disk}"
-#ifdef platform_rockchip
-EXTENV(partitions, ";name=bootloader,start=32K,size=131040K,uuid=\${uuid_gpt_bootloader}")
+#ifdef BOOTLOADER_PARTITION_OVERRIDE
+BOOTLOADER_PARTITION_OVERRIDE()
 #else
 EXTENV(partitions, ";name=bootloader,start=128K,size=130944K,uuid=\${uuid_gpt_bootloader}")
 #endif
@@ -62,6 +49,9 @@ EXTENV(bootargs, " firmware_class.path=/vendor/etc/firmware")
 EXTENV(bootargs, " ${debug_bootargs} printk.devkmsg=on")
 
 FUNC_BEGIN(enter_fastboot)
+#ifdef PRE_ENTER_FASTBOOT
+ PRE_ENTER_FASTBOOT()
+#endif
  setenv fastboot_fail 0
 #ifdef platform_sunxi
  /* OTG on sunxi require USB to be initialized */
@@ -128,36 +118,22 @@ FUNC_BEGIN(bootcmd_start)
  abootimg addr \$loadaddr \$vloadaddr
 
  adtimg addr \${dtboaddr}
-#ifdef platform_sunxi
-#ifdef device_pinephone
-/* Select proper DTS version for PinePhone (v1.1 or v1.2) */
- setenv main_fdt_id 0x11; /* -> PinePhone v1.1 */
- if test STRESC(\${fdtfile}) != STRESC(allwinner/sun50i-a64-pinephone-1.1.dtb);
- then
-  setenv main_fdt_id 0x12; /* -> PinePhone v1.2 */
- fi;
+#ifdef DEVICE_HANDLE_FDT
+ DEVICE_HANDLE_FDT()
 #endif
- adtimg get dt --id=\$main_fdt_id dtb_start dtb_size main_fdt_index &&
- cp.b \$dtb_start \$dtbaddr \$dtb_size &&
- fdt addr \$dtbaddr &&
-#endif
-
-#ifdef platform_rockchip
- adtimg get dt --id=\$main_fdt_id dtb_start dtb_size main_fdt_index &&
- cp.b \$dtb_start \$dtbaddr \$dtb_size &&
- fdt addr \$dtbaddr &&
+#ifdef PLATFORM_HANDLE_FDT
+ PLATFORM_HANDLE_FDT()
+#else
+#error PLATFORM_HANDLE_FDT is not defined
 #endif
 
 #ifdef platform_broadcom
-/* raspberrypi vc bootloader prepare fdt based on many factors. Use this fdt instead of dtb compiled by the kernel */
- fdt addr \${fdt_addr} &&
 #endif
  adtimg get dt --id=\$overlay_fdt_id dtb_start dtb_size overlay_fdt_index &&
  cp.b \$dtb_start \$dtboaddr \$dtb_size &&
  fdt resize 8192 &&
-#ifdef platform_rockchip
- fdt rsvmem add 0x8000000 0x8000000 &&
- fdt rsvmem print &&
+#ifdef POSTPROCESS_FDT
+ POSTPROCESS_FDT()
 #endif
  fdt apply \$dtboaddr &&
  FEXTENV(bootargs, " androidboot.dtbo_idx=\${main_fdt_index},\${overlay_fdt_index}") ;
@@ -167,16 +143,7 @@ FUNC_BEGIN(bootcmd_start)
 FUNC_END()
 
 FUNC_BEGIN(bootcmd_block)
-#ifdef device_pinephone
- if test STRESC(\$volume_key) = STRESC("up");
- then
-  run enter_fastboot ;
- fi;
- if test STRESC(\$volume_key) = STRESC("down");
- then
-  setenv androidrecovery true ;
- fi;
-#endif
+ DEVICE_HANDLE_BUTTONS()
  run bootcmd_bcb
  if test STRESC(\$androidrecovery) = STRESC("true");
  then

platform/uboot/platform_rockchip.h
@@ -1,0 +1,19 @@
+#pragma once
+
+#define PLATFORM_SETUP_ENV() \
+ setenv dtbaddr 0x1fa00000    \
+ setenv loadaddr 0x10008000   \
+ setenv vloadaddr 0x13008000  \
+ setenv dtboaddr 0x12008000   \
+
+#define BOOTLOADER_PARTITION_OVERRIDE() \
+ EXTENV(partitions, ";name=bootloader,start=32K,size=131040K,uuid=\${uuid_gpt_bootloader}")
+
+#define POSTPROCESS_FDT() \
+ fdt rsvmem add 0x8000000 0x8000000 && \
+ fdt rsvmem print &&                   \
+
+#define PLATFORM_HANDLE_FDT() \
+ adtimg get dt --id=\$main_fdt_id dtb_start dtb_size main_fdt_index && \
+ cp.b \$dtb_start \$dtbaddr \$dtb_size &&                              \
+ fdt addr \$dtbaddr &&                                                 \

platform/uboot/platform_sunxi.h
@@ -1,0 +1,12 @@
+#pragma once
+
+#define PLATFORM_SETUP_ENV() \
+ setenv dtbaddr 0x5fa00000
+ setenv loadaddr 0x50008000
+ setenv vloadaddr 0x53008000
+ setenv dtboaddr 0x52008000
+
+#define PLATFORM_HANDLE_FDT() \
+ adtimg get dt --id=\$main_fdt_id dtb_start dtb_size main_fdt_index && \
+ cp.b \$dtb_start \$dtbaddr \$dtb_size &&                              \
+ fdt addr \$dtbaddr &&                                                 \

platform/uboot/bootscript.h
@@ -1,6 +1,8 @@
 /* SPDX-License-Identifier: Apache-2.0 */
 /* (c) 2020 Roman Stratiienko r.stratiienko@gmail.com */
 
+#pragma once
+
 #define N(...) __VA_ARGS__
 #define Q() \"
 

platform/uboot/device_generic.h
@@ -1,0 +1,1 @@
+#pragma once

platform/uboot/platform_broadcom.h
@@ -1,0 +1,11 @@
+#pragma once
+
+#define PLATFORM_SETUP_ENV() \
+ setenv dtbaddr 0x1fa00000;   \
+ setenv loadaddr 0x10008000;  \
+ setenv vloadaddr 0x13008000; \
+ setenv dtboaddr 0x12008000;  \
+
+/* raspberrypi vc bootloader prepare fdt based on many factors. Use this fdt instead of dtb compiled by the kernel */
+#define PLATFORM_HANDLE_FDT() \
+ fdt addr \${fdt_addr} &&     \

platform/uboot/uboot.mk
@@ -116,11 +116,17 @@ endif
 
 BOOTSCRIPT_GEN := $(PRODUCT_OUT)/gen/BOOTSCRIPT/boot.txt
 
-$(BOOTSCRIPT_GEN): $(BSP_UBOOT_PATH)/bootscript.cpp $(BSP_UBOOT_PATH)/bootscript.h
+GD_BOOTSCRIPT ?= $(BSP_UBOOT_PATH)/bootscript.cpp
+GD_BOOTSCRIPT_OVERLAY_PLATFORM ?= $(BSP_UBOOT_PATH)/platform_$(PRODUCT_BOARD_PLATFORM).h
+GD_BOOTSCRIPT_OVERLAY_DEVICE ?= $(BSP_UBOOT_PATH)/device_generic.h
+
+$(BOOTSCRIPT_GEN): $(GD_BOOTSCRIPT) $(BSP_UBOOT_PATH)/bootscript.h $(GD_BOOTSCRIPT_OVERLAY_PLATFORM) $(GD_BOOTSCRIPT_OVERLAY_DEVICE)
 	mkdir -p $(dir $@)
-	$(CLANG) -E -P -Wno-invalid-pp-token $< -o $@ \
-	    -Dplatform_$(PRODUCT_BOARD_PLATFORM) \
-	    -Ddevice_$(PRODUCT_DEVICE) \
+	cp $(GD_BOOTSCRIPT) $(dir $(BOOTSCRIPT_GEN))/
+	cp $(BSP_UBOOT_PATH)/bootscript.h $(dir $(BOOTSCRIPT_GEN))/
+	cp $(GD_BOOTSCRIPT_OVERLAY_PLATFORM) $(dir $(BOOTSCRIPT_GEN))/platform.h
+	cp $(GD_BOOTSCRIPT_OVERLAY_DEVICE) $(dir $(BOOTSCRIPT_GEN))/device.h
+	$(CLANG) -E -P -Wno-invalid-pp-token $(dir $(BOOTSCRIPT_GEN))/$(notdir $<) -o $@ \
 	    -D__SYSFS_MMC0_PATH__=$(SYSFS_MMC0_PATH) \
 	    -D__SYSFS_MMC1_PATH__=$(SYSFS_MMC1_PATH) \
 

commit 0dcb10beeab53827c3d56800648b934b64417234

bootloader: Fix boostcript fragments
Fixes: 4880d9ac5538 ("bootscript: Rework to allow customization")
Signed-off-by: Roman Stratiienko <r.stratiienko@gmail.com>

platform/uboot/bootscript.cpp
@@ -143,7 +143,9 @@ FUNC_BEGIN(bootcmd_start)
 FUNC_END()
 
 FUNC_BEGIN(bootcmd_block)
+#ifdef DEVICE_HANDLE_BUTTONS
  DEVICE_HANDLE_BUTTONS()
+#endif
  run bootcmd_bcb
  if test STRESC(\$androidrecovery) = STRESC("true");
  then

platform/uboot/platform_rockchip.h
@@ -1,10 +1,10 @@
 #pragma once
 
 #define PLATFORM_SETUP_ENV() \
- setenv dtbaddr 0x1fa00000    \
- setenv loadaddr 0x10008000   \
- setenv vloadaddr 0x13008000  \
- setenv dtboaddr 0x12008000   \
+ setenv dtbaddr 0x1fa00000;   \
+ setenv loadaddr 0x10008000;  \
+ setenv vloadaddr 0x13008000; \
+ setenv dtboaddr 0x12008000;  \
 
 #define BOOTLOADER_PARTITION_OVERRIDE() \
  EXTENV(partitions, ";name=bootloader,start=32K,size=131040K,uuid=\${uuid_gpt_bootloader}")

platform/uboot/platform_sunxi.h
@@ -1,10 +1,10 @@
 #pragma once
 
 #define PLATFORM_SETUP_ENV() \
- setenv dtbaddr 0x5fa00000
- setenv loadaddr 0x50008000
- setenv vloadaddr 0x53008000
- setenv dtboaddr 0x52008000
+ setenv dtbaddr 0x5fa00000;
+ setenv loadaddr 0x50008000;
+ setenv vloadaddr 0x53008000;
+ setenv dtboaddr 0x52008000;
 
 #define PLATFORM_HANDLE_FDT() \
  adtimg get dt --id=\$main_fdt_id dtb_start dtb_size main_fdt_index && \