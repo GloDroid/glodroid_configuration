7a6a4ecfb6c79ee777a3e8167211c81082974c35
Roman Stratiienko <r.stratiienko@gmail.com>
2023-04-07 13:01:55 +0300

    codecs: Add dav1d project
    Signed-off-by: Roman Stratiienko <r.stratiienko@gmail.com>

--- common/codecs/board.mk
+++ common/codecs/board.mk
@@ -3,3 +3,6 @@
 # GloDroid project (https://github.com/GloDroid)
 #
 # Copyright (C) 2022 Roman Stratiienko (r.stratiienko@gmail.com)
+
+BOARD_BUILD_AOSPEXT_DAV1D := true
+BOARD_DAV1D_SRC_DIR := glodroid/vendor/dav1d
