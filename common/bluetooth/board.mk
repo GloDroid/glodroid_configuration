4d4cf95da7eb1d73de071a1bbefed2fef4595225
Roman Stratiienko <r.stratiienko@gmail.com>
2024-10-25 12:56:49 +0300

    common: Switch Bluetooth HAL to AIDL default
    Android-15 does not include a legacy one.
    
    Not tested.
    
    Signed-off-by: Roman Stratiienko <r.stratiienko@gmail.com>

--- common/bluetooth/board.mk
+++ common/bluetooth/board.mk
@@ -1,8 +1,3 @@
 # SPDX-License-Identifier: Apache-2.0
 
-# Some framework code requires this to enable BT
-
 BCB_PATH := $(patsubst $(CURDIR)/%,%,$(dir $(abspath $(lastword $(MAKEFILE_LIST)))))
-
-BOARD_HAVE_BLUETOOTH := true
-DEVICE_MANIFEST_FILE += $(BCB_PATH)/android.hardware.bluetooth@1.1.xml
